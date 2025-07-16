71ae305141059dde52ef6704952344d87e856668
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-07-16 23:00:20 +0300

    say HI to animations mother father

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,13 +1,25 @@
 import ProductDetails from './ProductDetails';
 import ProductGallery from './ProductGallery';
 import './ProductSection.css';
+import { motion } from 'motion/react';
 
 function ProductSection(props) {
 	return (
-		<section className="product-section">
+		<motion.section
+			className="product-section"
+			initial={{ opacity: 0, y: 150 }}
+			animate={{
+				opacity: 1,
+				y: 0,
+			}}
+			transition={{
+				duration: 0.35,
+				type: 'spring',
+				stiffness: 110,
+			}}>
 			<ProductGallery productImages={props.product.images} />
 			<ProductDetails {...props} />
-		</section>
+		</motion.section>
 	);
 }
 
