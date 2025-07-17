294739ca72439bc351fa1963614c7855d4fe9080
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2025-07-17 08:53:15 +0300

    Added Basic UI of Product Gallery

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import './ProductGallery.css';
 
-function ProductGallery() {
+function ProductGallery({ productImages }) {
+	const [selectedImg, setSelectedImg] = useState(productImages[0]);
+
+	const handleBtnClick = function (image) {
+		setSelectedImg(image);
+	};
+
+	const figures = productImages.map((image, index) => (
+		<button
+			key={index}
+			className={`btn btn--gallery  ${selectedImg === image ? 'btn--gallery--selected' : ''} `}
+			onClick={() => handleBtnClick(image)}>
+			<img src={image.thumbnail} alt={`Product Image ${index + 1}`} />
+		</button>
+	));
+
 	return (
 		<div className="gallery-container">
 			<figure className="gallery-container__main-img">
-				<img src={null} alt="" />
+				<img src={selectedImg.desktop} alt="Product Image" />
 			</figure>
-			<div className="gallery-container__sec-imgs">
-				<figure className="gallery-container__sec-img"></figure>
-				<figure className="gallery-container__sec-img"></figure>
-				<figure className="gallery-container__sec-img"></figure>
-				<figure className="gallery-container__sec-img"></figure>
-			</div>
+			<div className="gallery-container__sec-imgs">{figures}</div>
 		</div>
 	);
 }
